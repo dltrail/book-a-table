2c2c01cbd3347273c7ba0d0eba58bff3ef982e85
Dawn <dtrail76@gmail.com>
2024-09-24 19:40:51 +0100

    update no results component

--- src/components/NoResults.tsx
+++ src/components/NoResults.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
 
 interface NoResultsProps {
-searchTerm: string
+    searchTerm: string;
 }
 
-const NoResults: React.FC<NoResultsProps> = ({searchTerm}) => {
-  return (
-<div className="absolute bg-white z-[99] h-full w-full max-w-[1320px] text-center">
-           <h3>I couldn`t find anything based on your search of </h3>
-            <p className="font-bold text-lg">" {searchTerm} "</p> 
-            <p>   I`m not that sophistacated just yet.
-          </p>
+const NoResults: React.FC<NoResultsProps> = ({ searchTerm }) => {
+    return (
+        <div className="absolute bg-white z-[99] h-full w-full max-w-[1320px] text-center">
+            <h3>Sorry, we couldn`t find anything based on your search of </h3>
+            <p className="font-bold text-lg">" {searchTerm} "</p>
         </div>
-  );
+    );
 };
 
 export default NoResults;
