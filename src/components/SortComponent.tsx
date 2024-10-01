22fe8a368c3160126d0139fa77ca251d9dc3fc6e
Dawn <dtrail76@gmail.com>
2024-10-01 07:38:04 +0100

    Clean up sort styles

--- src/components/SortComponent.tsx
+++ src/components/SortComponent.tsx
@@ -10,9 +10,9 @@ const SortComponent: React.FC<SortProps> = ({
 }) => {
 
     return (
-        <div className="text-right mb-4">
-            <label htmlFor="sort">Sort by: </label>
-            <select className='border-[1px] p-2' name="sort" onChange={(e) => onSortOrderChange(e.target.value)}>
+        <div className="text-right mb-4 flex items-center">
+            <label htmlFor="sort" className='w-1/2' >Sort by: &nbsp;</label>
+            <select className='border-[1px] p-2 w-1/2' name="sort" onChange={(e) => onSortOrderChange(e.target.value)}>
                 <option value="asc">Lowest rating</option>
                 <option value="desc">Highest rating</option>
             </select>
